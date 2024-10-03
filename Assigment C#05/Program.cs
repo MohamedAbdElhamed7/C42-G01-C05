27f31d1a0d753352e033ebe63c482abae9a87965
MohamedAbdElhamed7 <mohamedabdalhamed845@gmail.com>
2024-10-04 02:46:34 +0300

    Qusetion 2

--- Assigment C#05/Program.cs
+++ Assigment C#05/Program.cs
@@ -57,6 +57,57 @@
 
 
             #endregion
+
+            #region Q2
+            //Explain the difference between passing(Reference type parameters) by value and
+            //by reference then write a suitable c# example.
+
+
+
+            //In passing(Reference type parameters) by value any if we chage the array (reference type)
+            //that change happend on new array stcakform so it will give accurate anser but if you want
+            // to use smothing from the array it will happend on the orginal array before
+            // any changing in fuction becouse that changed happedn on the array in the function
+            //happend on a copy from the array not the orginal array
+
+            //In passing(Reference type parameters) by ref any if we chage the array (reference type)
+            //the changed happend on the orginal array 
+
+
+            ////example for passing(Reference type parameters) by value :-
+            //---------------------------------------------------------
+            // static int SumArray(int[] Arr)
+            //{
+            //    int sum = 0;
+            //    Arr = new int[] { 4, 5, 6 };
+            //    for(int i=0; i<Arr.Length; i++)
+            //        sum += Arr[i];
+            //    return sum;
+
+            //} 
+
+            //int[] Numbers = { 1, 2, 3 };
+            //Console.WriteLine(SumArray(Numbers)); // 15
+            //Console.WriteLine(Numbers[0]); // 1
+
+
+            //example for passing(Reference type parameters) by refernce:-
+            //-----------------------------------------------------------
+
+            //static int SumArraybyref(ref int[] Arr)
+            //{
+            //    int sum = 0;
+            //    Arr = new int[] { 4, 5, 6 };
+            //    for(int i=0; i<Arr.Length; i++)
+            //       sum += Arr[i];
+            //    return sum;
+            //}
+
+            //int[] Numbers = { 1, 2, 3 };
+            //Console.WriteLine(SumArraybyref(ref Numbers)); // 15
+            //Console.WriteLine(Numbers[0]); // 4
+
+            #endregion
         }
     }
 }
